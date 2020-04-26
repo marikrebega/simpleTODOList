56ddb559b02eec18dc858cb74034bc2efbb90d82
marikrebega <marikrebega@gmail.com>
2020-04-26 22:09:05 +0300

    Edit conectDB

--- connectDB.php
+++ connectDB.php
@@ -1,4 +1,4 @@
 <?php
-	$dsn = 'mysql:host=localhost;dbname=tododb';
-	$pdo = new PDO( $dsn, 'mysql', 'mysql' );
+	$dsn = 'mysql:host=nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=h6541yngq9fdjcsd';
+	$pdo = new PDO( $dsn, 'mysimbwq1gk5bhwm', 'svth6ixkmw0w8aha');
 ?>
